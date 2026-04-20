6dbc0818a10521520e56860b5484ed3836f68bd5
Paulo Cidro <paulocidro@gmail.com>
2026-04-20 12:48:42 -0300

    implementando o formulario de login com signal forms

--- src/app/features/authentication/pages/login-form/login-form.ts
+++ src/app/features/authentication/pages/login-form/login-form.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { email, form, minLength, required, Field } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  imports: [Field],
   templateUrl: './login-form.html',
   styleUrl: './login-form.css',
 })
 export class LoginForm {
+  loginModel = signal({
+    email: '',
+    password: '',
+  });
 
+  loginForm = form(this.loginModel, (fieldpath) => {
+    required(fieldpath.email, { message: 'Email é obrigatório' });
+    email(fieldpath.email, { message: 'O email está invalido' });
+    required(fieldpath.password, { message: 'Senha é obrigatória' });
+    minLength(fieldpath.password, 8, { message: 'A senha deve ter no minimo 8 caracteres' });
+  });
 }
